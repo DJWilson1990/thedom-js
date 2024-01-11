a7ed25358b9dfd2a5546485e19e99342e2241aec
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-11 14:07:50 +0000

    lets hope this saves

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hello Dan");
+
+console.log("hello");
+
+const body = document.querySelector("body");
+
+console.log(body);
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = "Hello world!";
+
+const image = document.querySelector("img");
+
+image.src = "images/p2.jpg";
